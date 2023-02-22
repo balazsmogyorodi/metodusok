72e133213eed07f7e4a70b909c9ce9caca46d2b7
balazsmogyorodi <balazsmogyorodi@gmail.com>
2023-02-22 14:39:14 +0100

    javitot metodus

--- src/tombok/metodusok.java
+++ src/tombok/metodusok.java
@@ -4,29 +4,15 @@ package tombok;
 public class metodusok {
     
     public static void main(String[] args) {
-      int a = 10;
-      int b = 12;
-      
-      elso10Szamosszege();
-        osszead(a, b);
-        
-        
-        
-        
-        
-        
-       
-        
+     
     }
 
     private static void elso10Szamosszege() {
         int osszeg = 0;
         for (int i = 0; i < 10; i++) {
             osszeg += i;
-        
-        
         }
-        System.out.println("Az első 10 szám összege: " + osszeg);
+        kiir("Az első 10 szám összege: " + osszeg);
     }
 
     private static void osszead(int a, int b) {
@@ -34,6 +20,13 @@ public class metodusok {
         
         System.out.printf("%d + %d = %d\n", a, b,  a+b);
         
+    }
+    
+    private static void kiir(String szoveg) {
+        
+     System.out.println(szoveg);
+    
+    
     }
     
    
