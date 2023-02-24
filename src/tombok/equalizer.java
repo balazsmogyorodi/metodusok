b8d517b13a1ac8fa072921f6707bf9f67be23909
balazsmogyorodi <balazsmogyorodi@gmail.com>
2023-02-24 12:00:48 +0100

    fix and random

--- src/tombok/equalizer.java
+++ src/tombok/equalizer.java
@@ -1,15 +1,47 @@
 
 package tombok;
+import java.util.Random;
 
 public class equalizer {
     
+     private static Random rnd = new Random();
+    
      public static void main(String[] args) {
          
+     
          
-         
-         
+         equalizer_5_sorbol();
          
          
      }
-    
+
+    private static void equalizer_5_sorbol() {
+        int db = 0;
+        db = rnd.nextInt(1, 8);
+        if (db == db) {
+            db = rnd.nextInt(1, 8);
+        }
+        
+        eq(db);
+        eq(12);
+        eq(db);
+        eq(8);
+        eq(db);
+    }
+         
+         private static void eq(int db) {
+             
+             String szin = "\u001B[45m";
+             for (int i = 0; i < db; i++) {
+                 System.out.print(szin + " ");
+                 
+             }
+             System.out.println("");
+         
+         
+         
+         }
+         
+         
+     
 }
